7a9c0218fe479b1faebd104bdee31c042dbcaa86
DamascusKraken <DamascusKraken@gmail.com>
2023-09-11 18:34:43 -0500

    Finished numeric selection. Adds special characters and generates the desired password.

--- Develop/script.js
+++ Develop/script.js
@@ -22,7 +22,7 @@ function generatePassword(){
   var criteria = "";
 
   //variable userValid used as trigger to validate user input for password desired length.
-var userValid;
+  var userValid;
   //while loop to validate user input
   do{
     console.log(userValid);
@@ -41,24 +41,24 @@ var userValid;
       alert("Please enter a numeric value between 8 - 128? Thank you.")
     }
   
-   } while (userValid = true );
+  } while (userValid = true );
   
-//Users desire to add Upper case characters
-    userValid = true;
+  //Users desire to add Upper case characters
+  userValid = true;
   do{
     //Obtains users desire for upper case characters in password
-    var upperCase = prompt("Would you like to include UPPER CASE characters in your password?" + 
+    var upperCase = prompt("Would you like to include UPPER CASE characters in your password? " + 
                             "Enter 'yes' or 'no'");
     //Variable to hold userInput and convert answer to all lowercase to validate.
     var upperValid = upperCase.toLowerCase();
 
     if(upperValid === "yes"){
-       userValid = false;
-       criteria = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-       break;
-    } else if (upperValid === "no"){
       userValid = false;
+      criteria = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       break;
+    } else if (upperValid === "no"){
+        userValid = false;
+        break;
     } else{
       alert("Please enter 'yes' or 'no'.");
     }
@@ -66,49 +66,74 @@ var userValid;
   
   } while (userValid = true );
 
-//Users desire to add Lower case characters
-do{
-  //Obtains users desire for lower case characters in password
-  var lowerCase = prompt("Would you like to include LOWER CASE characters in your password?" + 
-                          "Enter 'yes' or 'no'");
-  //Variable to hold userInput and convert answer to all lowercase to validate.
-  var lowerValid = lowerCase.toLowerCase();
+  //Users desire to add Lower case characters
+  do{
+    //Obtains users desire for lower case characters in password
+    var lowerCase = prompt("Would you like to include LOWER CASE characters in your password? " + 
+                            "Enter 'yes' or 'no'");
+    //Variable to hold userInput and convert answer to all lowercase to validate.
+    var lowerValid = lowerCase.toLowerCase();
 
-  if(lowerValid  === "yes"){
-    userValid = false;
-    criteria += "abcdefghijklmnopqrstuvwxyz";
+    if(lowerValid  === "yes"){
+      userValid = false;
+      criteria += "abcdefghijklmnopqrstuvwxyz";
+      break;
+    } else if (lowerValid  === "no"){
+      userValid = false;
     break;
-  } else if (lowerValid  === "no"){
-    userValid = false;
-   break;
-  } else{
-    alert("Please enter 'yes' or 'no'.");
+    } else{
+      alert("Please enter 'yes' or 'no'.");
+    }
+    
+
+    } while (userValid = true );
+
+  //Users desire to add Numeric characters
+  do{
+    //Obtains users desire for Numeric characters in password
+    var numerals = prompt("Would you like to include NUMERIC characters in your password? " + 
+                            "Enter 'yes' or 'no'");
+    //Variable to hold userInput and convert answer to all lowercase to validate.
+    var numValid = numerals.toLowerCase();
+
+    if(numValid === "yes"){
+      userValid = false;
+      criteria += "1234567890";
+      break;
+    } else if (numValid  === "no"){
+      userValid = false;
+      break;
+    } else{
+      alert("Please enter 'yes' or 'no'.");
+    }
+    
+
+    } while (userValid = true );
+  //Users desire to add Special characters
+  do{
+    //Obtains users desire for Special characters in password
+    var specialChar = prompt("Would you like to include Special characters in your password? " + 
+                            "Enter 'yes' or 'no'");
+    //Variable to hold userInput and convert answer to all lowercase to validate.
+    var specialValid = specialChar.toLowerCase();
+
+    if(specialValid === "yes"){
+      userValid = false;
+      criteria += " !#$%&'()*+,-./:;<+>?@[\\]^_`{|}~";
+      break;
+    } else if (specialValid  === "no"){
+      userValid = false;
+      break;
+    } else{
+      alert("Please enter 'yes' or 'no'.");
+    }
+    
+
+  } while (userValid = true );
+  var randomChar = "";
+  for(var i = 0; i < userLength; i++){
+    randomChar += criteria.charAt(Math.floor(Math.random() * criteria.length));
   }
-  
 
-} while (userValid = true );
-
-//Users desire to add Numeric characters
-do{
-  //Obtains users desire for Numeric characters in password
-  var numerals = prompt("Would you like to include NUMERIC characters in your password?" + 
-                          "Enter 'yes' or 'no'");
-  //Variable to hold userInput and convert answer to all lowercase to validate.
-  var numValid = numerals.toLowerCase();
-
-  if(numValid === "yes"){
-     userValid = false;
-    return criteria += "1234567890";
-  } else if (lowerValid  === "no"){
-    userValid = false;
-    return "user has declined the use of numeric characters."
-  } else{
-    alert("Please enter 'yes' or 'no'.");
-  }
-  
-
-} while (userValid = true );
-//Users desire to add Special characters
-  
-  
-  }
+  return randomChar;
+}
